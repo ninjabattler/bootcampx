ea698b2cccd0aade08f18549bbb20840753105cb
ninjabattler <jaydenrtucker@gmail.com>
2020-09-17 01:26:28 +0000

    Updated scripts to use parameterized queries instead

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT teachers.name as teacher, cohorts.name as cohort
 FROM cohorts
 JOIN students ON cohorts.id = students.cohortId
 JOIN assistance_requests ON students.id = assistance_requests.student_id
 JOIN teachers ON assistance_requests.teacher_id = teachers.id
-WHERE cohorts.name LIKE ('%${process.argv[2]}%')
+WHERE cohorts.name LIKE ($1)
 GROUP BY teachers.name, cohorts.name
-`)
+`
+
+const values = [`%${process.argv[2]}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
